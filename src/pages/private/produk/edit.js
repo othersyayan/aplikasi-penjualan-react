f1c06ffcf0b01d666ff10cc94bb33508f1037aba
YanHD <others.handani@gmail.com>
2020-01-10 17:38:35 +0700

    add upload feature and edit setting #3

--- src/pages/private/produk/edit.js
+++ src/pages/private/produk/edit.js
@@ -5,6 +5,8 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import UploadIcon from '@material-ui/icons/CloudUpload';
+import SaveIcon from '@material-ui/icons/Save';
 
 // Import Firebase
 import {useFirebase} from '../../../components/FirebaseProvider';
@@ -12,52 +14,72 @@ import {useDocument} from 'react-firebase-hooks/firestore';
 
 // Import Page Loading
 import AppPageLoading from '../../../components/AppPageLoading';
+import { useSnackbar } from 'notistack';
+
+// Import Styles
+import useStyles from './edit.styles';
+
+import {Prompt} from 'react-router-dom';
 
 function EditProduk({match}) {
-    const {firestore, user} = useFirebase();
+    const classes = useStyles();
+
+    const {firestore, user, storage} = useFirebase();
+
+    const {enqueueSnackbar} = useSnackbar();
 
     const produkDoc = firestore.doc(`toko/${user.uid}/produk/${match.params.produkId}`);
+
+    const produkStorageRef = storage.ref(`toko/${user.uid}/produk/${match.params.produkId}`);
 
     const [snapshot, loading] = useDocument(produkDoc);
 
     const [form, setForm] = useState({
         nama: '',
-        sku:'',
-        harga: 0,
-        stok: 0,
+        sku: '',
+        harga:0,
+        stok:0,
         deskripsi: ''
-    })
+    });
 
     const [error, setError] = useState({
-        nama: '',
+        nama:'',
         sku: '',
         harga: '',
         stok: '',
         deskripsi: ''
     })
 
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const [isSomethingChange, setSomethingChange] = useState(false);
+
     useEffect(()=>{
+
         if (snapshot) {
-            setForm({
-                ...form,
+            setForm(currentForm => ({
+                ...currentForm,
                 ...snapshot.data()
-            });
+            }));
         }
+
     }, [snapshot])
 
-    const handleChange = e => {
+    const handleChange = e =>{
         setForm({
             ...form,
-            [e.target.name]: e.target.value
+            [e.target.name]:e.target.value
         })
 
         setError({
             ...error,
-            [e.target.name]: ''
+            [e.target.name]:''
         })
+
+        setSomethingChange(true);
     }
 
-    const validate = () => {
+    const validate = ()=>{
         const newError = {...error};
 
         if (!form.nama) {
@@ -75,13 +97,90 @@ function EditProduk({match}) {
         return newError;
     }
 
-    const handleSubmit = async e => {
+    const handleSubmit = async e =>{
         e.preventDefault();
 
-        const findErrors = validate();
+        const findError = validate();
 
-        if (Object.values(findErrors).some(err => err !== '')) {
-            setError(findErrors);
+        if (Object.values(findError).some(err => err !== '')) {
+            setError(findError);
+        } else {
+            try {
+                setIsSubmitting(true)
+                await produkDoc.set(form, {merge:true});
+                enqueueSnackbar('Data Produk berhasil disimpan', {variant:'success'});
+                setSomethingChange(false);
+            } catch (e) {
+                enqueueSnackbar(e.message, {variant:'error'})
+            }
+            setIsSubmitting(false)
+        }
+    }
+
+    const handleUploadFile = async (e)=>{
+        const file = e.target.files[0];
+
+        if (!['image/png', 'image/jpeg'].includes(file.type)) {
+            
+            setError(error=>({
+                ...error,
+                foto: `Tipe file tidak didukung : ${file.type}`
+            }))
+
+        } else if (file.size >= 512000) {
+
+            setError(error=>({
+                ...error,
+                foto: `Ukuran file terlalu besar, maksimal 500KB`
+            }))
+
+        } else {
+            
+            const reader = new FileReader();
+
+            reader.onabort = ()=>{
+                setError(error=>({
+                    ...error,
+                    foto: 'Proses pembacaan file dibatalkan'
+                }))
+            }
+
+            reader.onerror = ()=>{
+                setError(error=>({
+                    ...error,
+                    foto: 'File tidak bisa dibaca'
+                }))
+            }
+
+            reader.onload = async ()=>{
+                setError(error=>({
+                    ...error,
+                    foto: ''
+                }))
+                setIsSubmitting(true);
+                try {
+                    const fotoExt = file.name.substring(file.name.lastIndexOf('.'));
+                    const fotoRef = produkStorageRef.child(`${match.params.produkId}${fotoExt}`);
+                    const fotoSnapshot = await fotoRef.putString(reader.result, 'data_url');
+                    const fotoUrl = await fotoSnapshot.ref.getDownloadURL();
+
+                    setForm(currentForm=>({
+                        ...currentForm,
+                        foto:fotoUrl
+                    }));
+
+                    setSomethingChange(true);
+                } catch (e) {
+                    setError(error=>({
+                        ...error,
+                        foto: e.message
+                    }))
+                }
+                setIsSubmitting(false);
+            }
+
+            reader.readAsDataURL(file);
+
         }
     }
 
@@ -99,84 +198,121 @@ function EditProduk({match}) {
                 <form id="produk-form" onSubmit={handleSubmit} noValidate>
                     <TextField
                         id="nama"
-                        value="nama"
+                        name="nama"
                         label="Nama Produk"
                         margin="normal"
                         fullWidth
                         required
                         value={form.nama}
                         onChange={handleChange}
-                        error={error.nama?true:false}
                         helperText={error.nama}
+                        error={error.nama?true:false}
+                        disabled={isSubmitting}
                     />
                     <TextField
                         id="sku"
-                        value="sku"
+                        name="sku"
                         label="SKU Produk"
                         margin="normal"
                         fullWidth
                         value={form.sku}
                         onChange={handleChange}
+                        helperText={error.sku}
                         error={error.sku?true:false}
+                        disabled={isSubmitting}
                     />
                     <TextField
                         id="harga"
-                        value="harga"
+                        name="harga"
                         label="Harga Produk"
                         type="number"
-                        required
                         margin="normal"
                         fullWidth
+                        required
                         value={form.harga}
                         onChange={handleChange}
-                        error={error.harga?true:false}
                         helperText={error.harga}
+                        error={error.harga?true:false}
+                        disabled={isSubmitting}
                     />
                     <TextField
                         id="stok"
-                        value="stok"
+                        name="stok"
                         label="Stok Produk"
                         type="number"
                         margin="normal"
-                        required
                         fullWidth
+                        required
                         value={form.stok}
                         onChange={handleChange}
-                        error={error.stok?true:false}
                         helperText={error.stok}
+                        error={error.stok?true:false}
+                        disabled={isSubmitting}
                     />
                     <TextField
                         id="deskripsi"
-                        value="deskripsi"
+                        name="deskripsi"
                         label="Deskripsi Produk"
                         margin="normal"
                         multiline
                         rowsMax={3}
                         fullWidth
-                        value={form.nama}
+                        value={form.deskripsi}
                         onChange={handleChange}
-                        error={error.nama?true:false}
+                        helperText={error.deskripsi}
+                        error={error.deskripsi?true:false}
+                        disabled={isSubmitting}
                     />
                 </form>
             </Grid>
             <Grid item xs={12} sm={6}>
-                <Typography>
-                    Upload Gambar
-                </Typography>
+                <div className={classes.uploadFotoProduk}>
+                    {form.foto &&
+                        <img src={form.foto} className={classes.previewFotoProduk} alt={`Foto Produk ${form.nama}`}/>}
+                    <input
+                        className={classes.hideInputFile}
+                        type="file"
+                        id="upload-foto-produk"
+                        accept="image/jpeg,image/png"
+                        onChange={handleUploadFile}
+                    />
+                    <label
+                        htmlFor="upload-foto-produk"
+                    >
+                        <Button
+                            variant="outlined"
+                            component="span"
+                            disabled={isSubmitting}
+                        >
+                            Upload Foto <UploadIcon className={classes.iconRight}/>
+                        </Button>
+                    </label>
+                    {error.foto &&
+                        <Typography color="error">
+                        {error.foto}
+                    </Typography>}
+                </div>
             </Grid>
             <Grid item xs={12}>
-                <Button
-                    color="primary"
-                    variant="contained"
-                    type="submit"
-                    form="produk-form"
-                >
-                    Simpan
-                </Button>
+                <div className={classes.actionButton}>
+                    <Button
+                        color="primary"
+                        variant="contained"
+                        type="submit"
+                        form="produk-form"
+                        disabled={isSubmitting || !isSomethingChange}
+                    >
+                        <SaveIcon className={classes.iconLeft}/>
+                        Simpan
+                    </Button>
+                </div>
             </Grid>
         </Grid>
+        <Prompt
+            when={isSomethingChange}
+            message="Terdapat perubahan yang belum disimpan, apakah anda yakin ingin meninggalkan halaman ini ?"
+        />
     </div>
-
 }
 
 export default EditProduk;
